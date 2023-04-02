bf0a595e194e7b19ef431d8f55c7e62cac83fea7
Kevin <jkevincodes@gmail.com>
2023-04-02 13:31:48 -0600

    scripts: add fnm installer

---
+++ scripts/install-fnm.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# https://github.com/Schniz/fnm
+
+VERSION=$(curl -s "https://api.github.com/repos/Schniz/fnm/releases/latest" | grep -Po '"tag_name": "\K[^"]*')
+curl -Lo fnm-linux.zip "https://github.com/Schniz/fnm/releases/download/${VERSION}/fnm-linux.zip"
+unzip fnm-linux.zip
+chmod +x fnm
+mv fnm ~/.local/bin
