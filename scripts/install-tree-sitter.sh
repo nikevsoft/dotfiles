9867e5596b9f5b1b8f54cfef5fc654cd8316a246
Kevin <jkevincodes@gmail.com>
2023-04-02 14:15:52 -0600

    scripts: add tree-sitter installer

---
+++ scripts/install-tree-sitter.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# https://github.com/tree-sitter/tree-sitter
+
+VERSION=$(curl -s "https://api.github.com/repos/tree-sitter/tree-sitter/releases/latest" | grep -Po '"tag_name": "\K[^"]*')
+curl -Lo tree-sitter.gz "https://github.com/tree-sitter/tree-sitter/releases/download/${VERSION}/tree-sitter-linux-x64.gz"
+gzip -d tree-sitter.gz
+install tree-sitter ~/.local/bin/
