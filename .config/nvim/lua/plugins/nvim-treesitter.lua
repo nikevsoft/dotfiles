10a1feede432d772d56787d472fbdf33b9651600
Kevin <jkevincodes@gmail.com>
2023-04-02 15:22:32 -0600

    nvim(plugins): replace tree-sitter help with vimdoc

--- .config/nvim/lua/plugins/nvim-treesitter.lua
+++ .config/nvim/lua/plugins/nvim-treesitter.lua
@@ -1,8 +1,11 @@
 return {
   "nvim-treesitter/nvim-treesitter",
   opts = function(_, opts)
+    -- https://github.com/LazyVim/LazyVim/pull/521
+    opts.ignore_install = { "help" }
+
     if type(opts.ensure_installed) == "table" then
-      vim.list_extend(opts.ensure_installed, { "json", "json5", "jsonc" })
+      vim.list_extend(opts.ensure_installed, { "json", "json5", "jsonc", "vimdoc" })
     end
   end,
 }
