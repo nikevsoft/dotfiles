e2a525d306908f9e5eb35b3d433c80ed7f1f7e70
Kevin <jkevincodes@gmail.com>
2023-04-05 19:54:52 -0600

    nvim(null-ls): fix issue for duplicate code actions

--- .config/nvim/lua/plugins/null-ls.lua
+++ .config/nvim/lua/plugins/null-ls.lua
@@ -3,6 +3,5 @@ return {
   opts = function(_, opts)
     local nls = require("null-ls")
     table.insert(opts.sources, nls.builtins.formatting.prettierd)
-    table.insert(opts.sources, require("typescript.extensions.null-ls.code-actions"))
   end,
 }
