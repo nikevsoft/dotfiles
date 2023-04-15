bfd4b4f2ad2fb19deca86cf5adf7e32ea35df0a8
Kevin <jkevincodes@gmail.com>
2023-04-14 21:46:10 -0600

    scripts: add starship installer

---
+++ scripts/install-starship.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# https://github.com/starship/starship
+
+VERSION=$(curl -s "https://api.github.com/repos/starship/starship/releases/latest" | grep -Po '"tag_name": "\K[^"]*')
+curl -Lo starship.tar.gz "https://github.com/starship/starship/releases/download/${VERSION}/starship-x86_64-unknown-linux-gnu.tar.gz"
+tar xf starship.tar.gz starship
+install starship ~/.local/bin/
+rm starship starship.tar.gz
