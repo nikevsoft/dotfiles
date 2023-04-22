071e378ad79788f7b68850f281fff0d7253edc67
Kevin <jkevincodes@gmail.com>
2023-04-22 14:50:29 -0600

    nvim(treesitter): remove fix for vim help

--- .config/nvim/lua/plugins/nvim-treesitter.lua
+++ .config/nvim/lua/plugins/nvim-treesitter.lua
@@ -1,11 +1,8 @@
 return {
   "nvim-treesitter/nvim-treesitter",
   opts = function(_, opts)
-    -- https://github.com/LazyVim/LazyVim/pull/521
-    opts.ignore_install = { "help" }
-
     if type(opts.ensure_installed) == "table" then
-      vim.list_extend(opts.ensure_installed, { "json", "json5", "jsonc", "vimdoc", "typescript", "tsx" })
+      vim.list_extend(opts.ensure_installed, { "json", "json5", "jsonc", "typescript", "tsx" })
     end
   end,
 }
