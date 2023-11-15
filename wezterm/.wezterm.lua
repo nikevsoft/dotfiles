9de7f50589200a92879dbfb4f86dfce495cfba27
Kevin <jkevincodes@gmail.com>
2023-11-14 19:25:41 -0700

    wezterm: update config

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -1,8 +1,9 @@
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 
 return {
-	term = 'wezterm',
-	color_scheme = 'Catppuccin Frappe',
-	font = wezterm.font 'MonoLisaVariableScript Nerd Font',
-	font_size = 11
+	term = "wezterm",
+	-- color_scheme = "Tokyo Night Moon",
+	color_scheme = "Catppuccin Frappe",
+	font = wezterm.font("MonoLisaVariableScript Nerd Font"),
+	font_size = 11,
 }
