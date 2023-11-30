ed0b5ef133ddc86be5f5d52336d633a487d7b9c6
Kevin <jkevincodes@gmail.com>
2023-11-29 19:00:50 -0700

    vim: center screen when scrolling

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -2,8 +2,8 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
-vim.opt.scrolloff = 15
-vim.opt.sidescrolloff = 15
+vim.opt.scrolloff = 999
+vim.opt.sidescrolloff = 999
 -- vim.opt.tabstop = 4
 -- vim.opt.softtabstop = 4
 -- vim.opt.shiftwidth = 4
