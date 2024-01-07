0180aeb34701f8ef1e98c985d588749d702410eb
Kevin <jkevincodes@gmail.com>
2024-01-07 11:57:15 -0700

    nvim: disable navic and notify

--- nvim/lua/plugins/disable.lua
+++ nvim/lua/plugins/disable.lua
@@ -1,4 +1,6 @@
 return {
   { "Exafunction/codeium.nvim", enabled = false },
   { "christoomey/vim-tmux-navigator", enabled = false },
+  { "SmiteshP/nvim-navic", enabled = false },
+  { "rcarriga/nvim-notify", enabled = false },
 }
