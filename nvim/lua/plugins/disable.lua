8f8b3b8ec3640ee103c40f2633af2dcaa2597d8d
Kevin <jkevincodes@gmail.com>
2024-01-07 18:05:49 -0700

    nvim: disable navic custom_bg instead of per highlight group

--- nvim/lua/plugins/disable.lua
+++ nvim/lua/plugins/disable.lua
@@ -1,6 +1,5 @@
 return {
   { "Exafunction/codeium.nvim", enabled = false },
   { "christoomey/vim-tmux-navigator", enabled = false },
-  { "SmiteshP/nvim-navic", enabled = false },
   { "rcarriga/nvim-notify", enabled = false },
 }
