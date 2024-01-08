ff3397f060839427061e43429cea23690c9c8594
Kevin <jkevincodes@gmail.com>
2024-01-07 18:38:26 -0700

    nvim: update options

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -2,10 +2,12 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
--- vim.opt.scrolloff = 999
--- vim.opt.sidescrolloff = 999
--- vim.opt.tabstop = 4
--- vim.opt.softtabstop = 4
--- vim.opt.shiftwidth = 4
--- vim.opt.expandtab = true
+vim.opt.backup = false
 vim.opt.swapfile = false
+
+vim.opt.scrolloff = 8
+
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
+vim.opt.updatetime = 50
